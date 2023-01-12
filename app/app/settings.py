59b9c41de2515ccdb2b77ca9f6e272c120916e22
iyasyr <68105840+iyasyr@users.noreply.github.com>
2023-01-12 15:09:38 +0200

    Added recipe image upload feature.

--- app/app/settings.py
+++ app/app/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = '/vol/web/media'
-STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = 'vol/web/media/'
+STATIC_ROOT = 'vol/web/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
